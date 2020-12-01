0b4bd7d764e2b902b4f649a19b5e9bc97afbe9f5
Gokduman <hgokduman@libertyglobal.com>
2020-12-01 11:27:55 +0100

    Pass target value as argument

--- Day01.py
+++ Day01.py
@@ -4,10 +4,10 @@ from itertools import permutations
 
 entries = list(map(int, AoC.GetListFromInput(1)))
 
-def Day01(length):
+def Day01(length, target=2020):
     combinations = list(permutations(entries, length)) 
     for x in combinations:
-        if sum(x) == 2020:
+        if sum(x) == target:
             return(np.prod(x))
         
 print(Day01(2))
