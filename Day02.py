dfb5b484a02b79e1dde4ab1c11dfbaacc7936a3f
Gokduman <hgokduman@libertyglobal.com>
2020-12-02 15:27:43 +0100

    optimize code

--- Day02.py
+++ Day02.py
@@ -1,17 +1,13 @@
 from AoC.Resource import Resource
 from AoC import Display
 
-data = Resource(2).SplitLines().RegEx("([0-9]+)\-([0-9]+) ([A-z])\: ([A-z]+)").Get()
+data = Resource(2).SplitLines().RegEx("([0-9]+)-([0-9]+) ([A-z]): ([A-z]+)").Get()
 
 def Day02(part=1):
     validPasswords = 0
-    for PolicyNrOne, PolicyNrTwo, PolicyChar, Password in data:
-        if part == 1:
-            if int(PolicyNrOne) <= Password.count(PolicyChar) <= int(PolicyNrTwo):
-                validPasswords += 1
-        elif part == 2:
-            if (Password[int(PolicyNrOne)-1] == PolicyChar) ^ (Password[int(PolicyNrTwo)-1] == PolicyChar):
-                validPasswords += 1
+    for DigitOne, DigitTwo, Char, Password in data:
+        validPasswords += 1 if part == 1 and int(DigitOne) <= Password.count(Char) <= int(DigitTwo) else 0
+        validPasswords += 1 if part == 2 and (Password[int(DigitOne)-1] == Char) ^ (Password[int(DigitTwo)-1] == Char) else 0
     return(validPasswords)
 
 Display.DisplayAnswers(Day02, [1, 2])
