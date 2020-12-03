d682b7db0a250f71fd6b48e8083c975972bb5170
Gokduman <hgokduman@libertyglobal.com>
2020-12-03 09:28:05 +0100

    display execution time

--- AoC/Display.py
+++ AoC/Display.py
@@ -1,4 +1,10 @@
+import datetime
+
 def DisplayAnswers(func, listOfArgs):
     answers = map(func, listOfArgs)
-    for i, val in enumerate(answers):
-        print(f"Part {i+1}: {val}")
+    for i, val in enumerate(listOfArgs):
+        exec_start = datetime.datetime.now()
+        answer = func(val)
+        exec_stop = datetime.datetime.now()
+        exec_duration = (exec_stop - exec_start).total_seconds()
+        print(f"Part {i+1}: {answer} - executed in {exec_duration} seconds.")
