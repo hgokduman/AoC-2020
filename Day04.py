9ba2b9d07b6cadfdf4f6f084e271898926cbb0e1
Gokduman <hgokduman@libertyglobal.com>
2020-12-04 23:35:36 +0100

    small improvements

--- Day04.py
+++ Day04.py
@@ -9,7 +9,7 @@ class Day04(Puzzle.Puzzle):
     PassportValidity = []
 
     def PrepareData(self):
-        self.data = self.Resource.Get().replace("\n\n", ";").replace("\n", " ").replace(";", "\n").split("\n")
+        self.data = self.Resource.Get().replace("\n\n", ";").replace("\n", " ").split(";")
         self.passports = list(map(lambda p: {x[0]:x[1] for x in [f.split(":") for f in p.strip().split(" ")]}, self.data))   
 
     def SolvePartOne(self):
@@ -27,14 +27,8 @@ class Day04(Puzzle.Puzzle):
                 p["ecl"] = p["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
                 p["pid"] = re.match("^[0-9]{9}$", p["pid"]) is not None
                 p["hcl"] = re.match("^#[0-9a-f]{6}$", p["hcl"]) is not None
-                try:
-                    match1, match2 = re.compile("([0-9]+)(in|cm)").findall(p["hgt"])[0]
-                    if ((match2 == "cm" and 150 <= int(match1) <= 193) or (match2 == "in" and 59 <= int(match1) <= 76)):
-                        p["hgt"] = True
-                    else:
-                        p["hgt"] = False
-                except Exception:
-                    p["hgt"] = False
+                p["hgt"] = False if re.match("([0-9]+)(in|cm)", p["hgt"]) is None else (int(p["hgt"][:-2]), p["hgt"][-2:])
+                p["hgt"] = False if p["hgt"] == False else (p["hgt"][1] == "cm" and 150 <= p["hgt"][0] <= 193) or (p["hgt"][1] == "in" and 59 <= p["hgt"][0] <= 76)
                 ValidPasswords += 1 if list(p.values()).count(False) == 0 else 0
 
         return(ValidPasswords)
