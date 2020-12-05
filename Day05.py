fa6bcf6bd1b80f2e2520f804a11b000ccf2d843c
Gokduman <hgokduman@libertyglobal.com>
2020-12-05 10:16:42 +0100

    Day 5

--- Day05.py
+++ Day05.py
@@ -9,13 +9,12 @@ class Day05(Puzzle.Puzzle):
     
     def PrepareData(self):
         self.data = self.Resource.SplitLines().Get()
-
+   
     def GetNumber(self, input):
-        Values = list(range(0, pow(2, len(input))))
-        for x in input:
-            HalfOfValues = int(len(Values)/2)
-            Values = Values[:HalfOfValues] if x in ["F", "L"] else Values[-HalfOfValues:]
-        return Values[0]
+        replacements = { "F": 0, "L": 0, "B": 1, "R": 1}
+        for x,y in replacements.items():
+            input = input.replace(x, str(y))
+        return int(input, 2)
 
     def GetSeatId(self, seat):
         return (seat[0] * 8 + seat[1])
@@ -26,11 +25,10 @@ class Day05(Puzzle.Puzzle):
             self.Seats.append((p, seat, self.GetSeatId(seat)))
         return (max([x for _, _, x in self.Seats]))
 
-
     def SolvePartTwo(self):
-        AllSeats = list(itertools.product(range(0, 128), range(0, 8)))
-        FreeSeats = [x for x in AllSeats if x not in [a for _, a, _ in self.Seats]]
-        MySeat = [x for x in FreeSeats if 10 <= x[0] <= 100][0]
-        return self.GetSeatId(MySeat)
+        AllSeats = list(map(self.GetSeatId, list(itertools.product(range(0, 128), range(0, 8)))))
+        OccupiedSeats = [x for x in AllSeats if x in [a for _, _, a in self.Seats]]
+        FreeSeats = [x for x in AllSeats if x not in OccupiedSeats]
+        return list(filter(lambda x: x-1 in OccupiedSeats and x+1 in OccupiedSeats, FreeSeats))[0]
 
 Display.DisplayAnswers(Day05(Resource(5)))
