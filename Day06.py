5f2483c3693dc15a33d72589d5577f557b0a9bed
Gokduman <hgokduman@libertyglobal.com>
2020-12-06 10:44:21 +0100

    small improvement

--- Day06.py
+++ Day06.py
@@ -7,15 +7,13 @@ import functools
 
 class Day06(Puzzle.Puzzle):
 
-
     def PrepareData(self):
         self.data = [x.strip().split(" ") for x in self.Resource.Get().replace("\n\n", ";").replace("\n", " ").split(";")]
-        #self.data = [x.strip().split(" ") for x in self.data]
 
     def SolvePartOne(self):
-        return functools.reduce(operator.add, [len(set("".join(x))) for x in self.data])
+        return np.sum([len(set("".join(x))) for x in self.data])
 
     def SolvePartTwo(self):
-        return functools.reduce(operator.add, [len(functools.reduce(operator.and_, [set(x) for x in p])) for p in [g for g in self.data]])
+        return np.sum([len(functools.reduce(operator.and_, [set(x) for x in p])) for p in [g for g in self.data]])
 
 Display.DisplayAnswers(Day06(Resource(6)))
