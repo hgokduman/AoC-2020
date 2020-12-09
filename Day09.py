1bc02298a1de3daeda9c36a558689a159904e334
Gokduman <hgokduman@libertyglobal.com>
2020-12-09 09:55:34 +0100

    Day9

---
+++ Day09.py
@@ -0,0 +1,36 @@
+from AoC.Resource import Resource
+from AoC import Display
+from AoC import Puzzle
+from itertools import combinations
+
+class Day09(Puzzle.Puzzle):
+    def PrepareData(self):
+        self.data = [int(x) for x in self.Resource.SplitLines().Get()]
+
+    def FindInvalidNumber(self, preambleSize):
+        for i in range(preambleSize, len(self.data)):
+            sets = [sum(x) for x in combinations(self.data[i-25:i], 2)]
+            if self.data[i] not in sets:
+                return (i, self.data[i])
+
+    def FindContiguousSet(self, InvalidNumber, Index):
+        dataset = self.data[:Index]
+
+        for i in range(Index):
+            for s in range(Index-1):
+                currentSet = dataset[i:s]
+                if sum(currentSet) == InvalidNumber:
+                    return currentSet
+
+        return None
+
+    def SolvePartOne(self):
+        _, Number = self.FindInvalidNumber(25)
+        return Number
+
+    def SolvePartTwo(self):
+        Index, Number = self.FindInvalidNumber(25)
+        ContiguousSet = self.FindContiguousSet(Number, Index)
+        return min(ContiguousSet)+max(ContiguousSet)
+                
+Display.DisplayAnswers(Day09(Resource(9)))
