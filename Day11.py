652e3241d17161daa7c9b7e497ec563aae8abb3b
Gokduman <hgokduman@libertyglobal.com>
2020-12-11 14:58:32 +0100

    clean up code

--- Day11.py
+++ Day11.py
@@ -1,6 +1,7 @@
 from AoC.Resource import Resource
 from AoC import Display, Puzzle
 import copy
+import itertools
 
 FLOOR = "."
 EMPTY = "L"
@@ -9,11 +10,15 @@ DIRECTIONS = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, 1), (1, -
 
 class Day11(Puzzle.Puzzle):
     grid = None
+    points = []
     gridWidth = 0
     gridHeight = 0
 
     def PrepareData(self):
         self.data = [x for x in self.Resource.SplitLines().Get()]
+        self.Reset()
+        self.gridHeight, self.gridWidth = len(self.grid), len(self.grid[0])
+        self.points = list(itertools.chain.from_iterable([[(x,y) for y in range(self.gridHeight)] for x in range(self.gridWidth)]))
 
     def OccupiedAdjacentSeats(self, grid, x, y):
         checkPositions = [(x+moveX, y+moveY) for moveX, moveY in DIRECTIONS if 0 <= x+moveX < self.gridWidth and 0 <= y+moveY < self.gridHeight ]
@@ -38,43 +43,33 @@ class Day11(Puzzle.Puzzle):
 
         return sum([grid[posY][posX] == OCCUPIED for posX, posY in checkPositions])
 
-    def ApplyRules(self, part):
-        Changes = 0
-        grid = copy.deepcopy(self.grid)
-        tolerance = 4 if part == 1 else 5
-        for y in range(self.gridHeight):
-            for x in range(self.gridWidth):
+    def Run(self, Part, Tolerance):
+        self.Reset()
+        Changed = None
+        while Changed != 0:
+            grid = copy.deepcopy(self.grid)
+            Changed = 0
+            for x,y in self.points:
                 seat = grid[y][x]
                 if not seat == FLOOR:
-                    OccupiedSeats = self.OccupiedAdjacentSeats(grid, x, y) if part == 1 else self.OccupiedVisibleSeats(grid, x, y)
+                    OccupiedSeats = self.OccupiedAdjacentSeats(grid, x, y) if Part == 1 else self.OccupiedVisibleSeats(grid, x, y)
                     if seat == EMPTY and OccupiedSeats == 0:
                         self.grid[y][x] = OCCUPIED
-                        Changes += 1
-                    elif seat == OCCUPIED and OccupiedSeats >= tolerance:
+                        Changed += 1
+                    elif seat == OCCUPIED and OccupiedSeats >= Tolerance:
                         self.grid[y][x] = EMPTY
-                        Changes += 1
-    
-        TotalOccupiedSeats = sum([sum([x == OCCUPIED for x in y]) for y in grid])
-        return (Changes, TotalOccupiedSeats)
+                        Changed += 1
+        
+        return sum([sum([x == OCCUPIED for x in y]) for y in grid])
 
     def SolvePartOne(self):
-        self.Reset()
+        return self.Run(1, 4)
 
-        while True:
-            Changes, Occupied = self.ApplyRules(1)
-            if Changes == 0:
-                return Occupied
-
-    def SolvePartTwo(self):
-        self.Reset()       
-        while True:
-            Changes, Occupied = self.ApplyRules(2)
-            if Changes == 0:
-                return Occupied
+    def SolvePartTwo1(self):
+        return self.Run(2, 5)
 
     def Reset(self):
         self.grid = [[s for s in x] for x in self.data]
-        self.gridHeight, self.gridWidth = len(self.grid), len(self.grid[0])
                 
 if __name__ == '__main__':
     Display.DisplayAnswers(Day11(Resource(11)))
