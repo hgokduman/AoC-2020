96a415ffdd673be30f416b4f74e7b1726c590b0b
Halim <halim@gokduman.nl>
2020-12-13 00:53:50 +0100

    clean up code

--- Day11.py
+++ Day11.py
@@ -18,20 +18,26 @@ class Day11(Puzzle.Puzzle):
         self.Reset()
         self.gridHeight, self.gridWidth = len(self.grid), len(self.grid[0])
         self.points = list(itertools.chain.from_iterable([[(x,y) for y in range(self.gridHeight)] for x in range(self.gridWidth)]))
+        self.seats = [(x,y) for x,y in self.points if self.grid[y][x] != FLOOR]
 
-    def OccupiedAdjacentSeats(self, grid, x, y):
-        checkPositions = [(x+moveX, y+moveY) for moveX, moveY in DIRECTIONS if 0 <= x+moveX < self.gridWidth and 0 <= y+moveY < self.gridHeight ]
-        return sum([grid[posY][posX] == OCCUPIED for posX, posY in checkPositions])
+    def OccupiedAdjacentSeats(self, x, y):
+        checkPositions = [(x+moveX, y+moveY) for moveX, moveY in DIRECTIONS if self.IsValidPosition(x+moveX, y+moveY)]
+        return sum([self.grid[posY][posX] == OCCUPIED for posX, posY in checkPositions])
 
-    def OccupiedVisibleSeats(self, grid, x, y):
+    def IsValidPosition(self, x, y):
+        return 0<=x<self.gridWidth and 0<=y<self.gridHeight
+
+    def IsSeat(self, x, y):
+        return self.IsValidPosition(x,y) and self.grid[y][x] != FLOOR #(x,y) in self.seats
+
+    def OccupiedVisibleSeats(self,  x, y):
         checkPositions = []
         def FindFirstSeat(directionX, directionY):
             posX, posY = x, y
             while True:
                 posX, posY = posX+directionX, posY+directionY
-                if 0 <= posX < self.gridWidth and 0 <= posY < self.gridHeight:
-                    if grid[posY][posX] != FLOOR:
-                        return (posX, posY)
+                if self.IsSeat(posX, posY):
+                    return (posX, posY)
                 else:
                     return None
 
@@ -40,32 +46,24 @@ class Day11(Puzzle.Puzzle):
             if seat is not None:
                 checkPositions.append((seat[0], seat[1]))
 
-        return sum([grid[posY][posX] == OCCUPIED for posX, posY in checkPositions])
+        return sum([self.grid[posY][posX] == OCCUPIED for posX, posY in checkPositions])
 
-    def Run(self, Part, Tolerance):
+    def Run(self, Part, Tolerance, checkFunc):
         self.Reset()
         Changed = None
         while Changed != 0:
-            grid = copy.deepcopy(self.grid)
-            Changed = 0
-            for x,y in self.points:
-                seat = grid[y][x]
-                if not seat == FLOOR:
-                    OccupiedSeats = self.OccupiedAdjacentSeats(grid, x, y) if Part == 1 else self.OccupiedVisibleSeats(grid, x, y)
-                    if seat == EMPTY and OccupiedSeats == 0:
-                        self.grid[y][x] = OCCUPIED
-                        Changed += 1
-                    elif seat == OCCUPIED and OccupiedSeats >= Tolerance:
-                        self.grid[y][x] = EMPTY
-                        Changed += 1
+            swap = [(x,y, self.grid[y][x]) for x,y in self.seats if (self.grid[y][x] == EMPTY and checkFunc(x, y) == 0) or (self.grid[y][x] == OCCUPIED and checkFunc(x, y) >= Tolerance)]
+            for x,y,seat in swap:
+                self.grid[y][x] = OCCUPIED if seat == EMPTY else EMPTY 
+            Changed = len(swap)
         
-        return sum([sum([x == OCCUPIED for x in y]) for y in grid])
+        return sum([sum([x == OCCUPIED for x in y]) for y in self.grid])
 
     def SolvePartOne(self):
-        return self.Run(1, 4)
+        return self.Run(1, 4, self.OccupiedAdjacentSeats)
 
     def SolvePartTwo(self):
-        return self.Run(2, 5)
+        return self.Run(2, 5, self.OccupiedVisibleSeats)
 
     def Reset(self):
         self.grid = [[s for s in x] for x in self.data]
