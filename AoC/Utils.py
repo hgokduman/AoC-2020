6870d314935cc21e4d5da1f3fe357c42809997d8
Halim <halim@gokduman.nl>
2020-12-13 10:26:46 +0100

    small fix

--- AoC/Utils.py
+++ AoC/Utils.py
@@ -2,9 +2,7 @@ import datetime
 import time
 
 def ShowAnswers(obj):
-    solutions = obj.Solve()
-    answer_max_length = max([len(str(x[0])) for x in solutions if x is not None])
-    for i, val in enumerate(solutions):
+    for i, val in enumerate(obj.Solve()):
         answer, exec_time = val
-        answer = format(answer, f"{answer_max_length}d")
-        print(f"Part {i+1}: {answer} in {exec_time:0.3f}ms")
+        if answer is not None:
+            print(f"Part {i+1}: {answer} in {exec_time:0.3f}ms")
