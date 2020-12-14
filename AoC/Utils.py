862ad577e4d1f6e2e9b2d18304984d6da30de47b
Halim <halim@gokduman.nl>
2020-12-14 09:09:36 +0000

    Replace multi function

--- AoC/Utils.py
+++ AoC/Utils.py
@@ -6,3 +6,13 @@ def ShowAnswers(obj):
         answer, exec_time = val
         if answer is not None:
             print(f"Part {i+1}: {answer} in {exec_time:0.3f}ms")
+
+def Replace(FindIn, FindStr, ReplaceWith):
+    if not isinstance(FindStr, list):
+        FindStr = [FindStr]
+
+    for i,r in enumerate(FindStr):
+        replacement = ReplaceWith if not isinstance(ReplaceWith, list) else ReplaceWith[i]
+        FindIn = FindIn.replace(r, replacement)
+
+    return FindIn
